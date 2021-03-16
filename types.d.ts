3342a7027765c4e2c65f8c4fb60f592e2b0b5802
JHurley-BD <jerome.hurley@northlink.digital>
2021-03-16 15:54:23 +0000

    Types and interfaces added

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,20 @@
+declare namespace NodeJS {
+	interface ProcessEnv {
+		NODE_ENV: "development" | "production" | "test";
+		PUBLIC_URL: string;
+		REACT_APP_HASH: string;
+		REACT_APP_API_URI: string;
+		REACT_APP_WS_URI: string;
+		REACT_APP_FIREBASE_API_KEY: string;
+		REACT_APP_FIREBASE_AUTH_DOMAIN: string;
+		REACT_APP_FIREBASE_PROJ_ID: string;
+		REACT_APP_FIREBASE_STORAGE_BUCKET: string;
+		REACT_APP_FIREBASE_MSG_SENDER_ID: string;
+		REACT_APP_FIREBASE_APP_ID: string;
+		REACT_APP_FIREBASE_MEASUREMENT_ID: string;
+	}
+}
+
 type IReducer<S, A> = (prevState: S, action: A) => S;
 type IDispatch<IAppAction> = (value: IAppAction) => void;
 
@@ -17,14 +34,28 @@ interface IAppAction {
 	payload: string;
 }
 
-interface IAppContextProvider {}
-
-declare namespace NodeJS {
-	interface ProcessEnv {
-		NODE_ENV: "development" | "production" | "test";
-		PUBLIC_URL: string;
-		REACT_APP_HASH: string;
-		REACT_APP_API_URI: string;
-		REACT_APP_WS_URI: string;
-	}
+interface Contact {
+	userId: string;
+	name: string;
+	avatar: string;
 }
+interface User {
+	id: string;
+	name: string;
+	avatar: string;
+	lastSeen: Date;
+	contacts: Contact[];
+}
+interface Message {
+	id: string;
+	sentBy: string;
+	channel: string;
+	type: string;
+	message: string;
+	fileUrl: string;
+	time: Date;
+}
+
+type FirestoreData = Message | User;
+
+type CollectionName = "users" | "messages";
