f39ed09a1415e97ea494b352bf95173e0bc04a65
JHurley-BD <jerome.hurley@northlink.digital>
2021-03-16 17:02:11 +0000

    loadUser added to reducer

--- src/reducers/reducer-app.ts
+++ src/reducers/reducer-app.ts
@@ -5,6 +5,10 @@ export const reducerApp: IReducer<State, Action> = (
 	switch (action.type) {
 		case "setAppName":
 			return { ...state, appName: action.payload };
+		case "loadUser":
+			const user = action.payload;
+			localStorage.setItem("whatsapp-clone-user", JSON.stringify(user));
+			return { ...state, user };
 		default:
 			console.error("reducer Error", { state }, { action });
 			return { ...state };
